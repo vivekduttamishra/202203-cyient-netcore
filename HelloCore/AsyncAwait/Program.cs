e8615de06fb254192d3de803367f5e507a0382ca
vivekduttamishra <vivek@conceptarchitect.in>
2022-03-19 16:20:18 +0530

    day#2 update#1

---
+++ HelloCore/AsyncAwait/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var task = CountPrimes(2, 200000);
+            Console.Write("Waiting for task to complete...");
+            while (!task.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Calculation is over");
+            Console.WriteLine("Total Primes "+task.Result);
+
+        }
+
+
+        static async Task<int> CountPrimes(int min, int max)
+        {
+            int count = 0;
+
+            for (int i = min; i < max; i++)
+            {
+                var result = await IsPrime(i);
+                if (result)
+                    count++;
+            }
+
+            return count;
+        }
+
+
+
+
+        static int CountPrimesSync(int min,int max)
+        {
+            int count = 0;
+
+            for(int i=min;i<max;i++)
+            {
+                var task = IsPrime(i);
+                task.Wait();
+                var result = task.Result;
+                if (result)
+                    count++;
+            }
+
+            return count;
+        }
+
+
+
+        static Task<bool> IsPrime(int number)
+        {
+            //let us create a task to this job
+
+            return Task.Factory.StartNew(() =>
+            {
+                if (number < 2)
+                    return false;
+                for (int i = 2; i < number; i++)
+                    if (number % i == 0)
+                        return false;
+
+                return true;
+            });
+
+        }
+    }
+}
---
+++ HelloCore/SimpleHttpServer/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleHttpServer.Models;
+
+namespace SimpleHttpServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public String handleLogin()
+        {
+            return "Login Successful on "+HttpContext.Request.Method+" call";
+        }
+
+        public String deleteUser()
+        {
+            if (HttpContext.Request.Method != "DELETE")
+                return "Delete request on " + HttpContext.Request.Method + " call is denied";
+            //write database logic to delete user.
+            return "Deleted user on " + HttpContext.Request.Method + " call";
+
+        }
+    }
+}
